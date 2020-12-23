f090c0f77d7258981fa5e8be853f8ba7b2b78c47
islam-asankojoev <islam.asankojoev@gmail.com>
2020-12-23 15:55:05 +0600

    Link of navmenu for courses page

--- news/urls.py
+++ news/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('news', NewsAll.as_view(), name="all_news"),
     path('news/<str:title>', newsByCategory, name='newsByCategory'),
     path('course', CourseList.as_view(), name='courseAll'),
-    path('API', all_api, name="apiById"),
+    path('all_api/', all_api, name="apiById"),
 ]
 
 urlpatterns += router.urls
